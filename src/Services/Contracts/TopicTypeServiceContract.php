643cee3a7cb50cc6904c3fd08bb2dcaf7dc11c0e
KD <do>
2022-04-13 17:35:36 +0200

    fix

--- src/Services/Contracts/TopicTypeServiceContract.php
+++ src/Services/Contracts/TopicTypeServiceContract.php
@@ -4,6 +4,8 @@ namespace EscolaLms\TopicTypes\Services\Contracts;
 
 interface TopicTypeServiceContract
 {
+    public static function sanitizePath(string $path): string;
+
     public function fixAssetPaths(): array;
 
     public function fixTopicTypeColumnName(): int;

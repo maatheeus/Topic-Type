cc159bf4374d3c5141a8925700a4e1b319865698
KD <do>
2022-04-13 16:09:17 +0200

    add tests

--- tests/Helpers/HelpersMethodTest.php
+++ tests/Helpers/HelpersMethodTest.php
@@ -6,6 +6,7 @@ use EscolaLms\Courses\Models\Course;
 use EscolaLms\Courses\Models\Lesson;
 use EscolaLms\Courses\Models\Topic;
 use EscolaLms\TopicTypes\Facades\Markdown;
+use EscolaLms\TopicTypes\Facades\Path;
 use EscolaLms\TopicTypes\Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Support\Facades\Storage;
@@ -26,7 +27,7 @@ class HelpersMethodTest extends TestCase
         ])->create();
     }
 
-    public function testConvertImagesApi()
+    public function testConvertImagesApi(): void
     {
         $topic = $this->topic;
         $course = $topic->lesson->course;
@@ -38,5 +39,19 @@ class HelpersMethodTest extends TestCase
         $this->assertArrayHasKey('results', $result);
         $this->assertTrue(is_array($result['results']));
         $this->assertTrue(isset($result['results'][0]) && is_array($result['results'][0]));
+    }
+
+    public function testGetImagesPathsWithoutImageApi(): void
+    {
+        $result = Markdown::getImagesPathsWithoutImageApi("![Image] (api/images/img?path=course/1234/topic/123/test.jpg)");
+
+        $this->assertEquals("![Image] (course/1234/topic/123/test.jpg)", $result);
+    }
+
+    public function testSanitizePathForExport(): void
+    {
+        $result = Path::sanitizePathForExport("courses/1234/topic/123/test.jpg");
+
+        $this->assertEquals('topic/123/test.jpg', $result);
     }
 }
